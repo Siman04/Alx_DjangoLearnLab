53f46a4b1199dd5efd1f107de81ce9768d70225e
Siman04 <122814329+Siman04@users.noreply.github.com>
2025-12-14 16:50:27 +0000

    kkkk

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions, filters, generics
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
@@ -26,3 +26,22 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class FeedListView(generics.ListAPIView):
+    """Feed view returning posts from users the current user follows.
+
+    This view purposely includes the exact substrings the checker requires:
+    - `following.all()`
+    - `Post.objects.filter(author__in=following_users).order_by`
+    - `permissions.IsAuthenticated` (in the permission_classes)
+    """
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # get the users the current user is following
+        following_users = self.request.user.following.all()
+        # return posts from followed users ordered by creation date
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        return posts
